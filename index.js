f8b4ec5a56c32d02d894c6b8b17ffe2e8aa27407
M Faris Gibran <mfarisgibran@gmail.com>
2020-11-02 18:22:33 +0700

    Create new function to show property's summary

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 class Property {
-  constructor(size, price, location) {
+  constructor(name, size, price, location) {
+    this.name = name
     this.size = size
     this.price = price
     this.location = location
   }
+  showSizeInSQM = () => {
+    return this.size + ' m2'
+  }
+  showPriceInIDR = () => {
+    return 'IDR ' + this.price
+  }
+  showSummary = () => {
+    return `${
+      this.name
+    } is ${this.showSizeInSQM()} in size and its price is ${this.showPriceInIDR()}. It is located in ${
+      this.location
+    }.`
+  }
 }
 
-const vasakaNines = new Property(60, 1400000000, 'BSD City')
-const vasakaSoltera = new Property(65, 1800000000, 'Pejaten')
+const vasakaNines = new Property('Vasaka Nines', 70, 1400000000, 'BSD City')
+const vasakaSoltera = new Property('Vasaka Soltera', 65, 1800000000, 'Pejaten')
 
-console.log(vasakaNines)
-console.log(vasakaSoltera)
+console.log(vasakaNines.showSummary())
+console.log(vasakaSoltera.showSummary())
