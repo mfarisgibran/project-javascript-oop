99a62dbf66c301586e99fdd0486f6a7472f3d1e0
M Faris Gibran <mfarisgibran@gmail.com>
2020-11-02 18:03:28 +0700

    Create property class and object

---
+++ index.js
@@ -0,0 +1,11 @@
+class Property {
+  constructor(size, price, location) {
+    this.size = size
+    this.price = price
+    this.location = location
+  }
+}
+
+const vasakaNines = new Property(60, 1400000000, 'BSD')
+
+console.log(vasakaNines)
