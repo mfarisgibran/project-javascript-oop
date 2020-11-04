4a9dd67c5d9f0cf4c43b418fb0591ff982a265f4
M Faris Gibran <mfarisgibran@gmail.com>
2020-11-04 20:10:09 +0700

    Inherit house from property

--- index.js
+++ index.js
@@ -20,8 +20,27 @@ class Property {
   }
 }
 
+class House extends Property {
+  constructor(name, size, price, location, story) {
+    super(name, size, price, location)
+    this.story = story
+  }
+  showNumberOfStories = () => {
+    return this.story + ' stories'
+  }
+  showSummary = () => {
+    return `${
+      this.name
+    } is ${this.showSizeInSQM()} in size and its price is ${this.showPriceInIDR()}. It is located in ${
+      this.location
+    } with ${this.showNumberOfStories()}.`
+  }
+}
+
 const vasakaNines = new Property('Vasaka Nines', 70, 1400000000, 'BSD City')
 const vasakaSoltera = new Property('Vasaka Soltera', 65, 1800000000, 'Pejaten')
+const avastaBekasi = new House('Avasta Bekasi', 200, 1500000000, 'Bekasi', 2)
 
 console.log(vasakaNines.showSummary())
 console.log(vasakaSoltera.showSummary())
+console.log(avastaBekasi.showSummary())
